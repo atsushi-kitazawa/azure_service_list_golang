21fda77aa80fb1a93028a0bc1bda89587306d680
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-05-19 00:23:12 +0900

    commit.

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/cdproto/dom"
 	"github.com/chromedp/chromedp"
 )
 
@@ -19,17 +21,45 @@ func doMain() {
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
-	var nodes []*cdp.Node
+	var serviceRows []*cdp.Node
+	var capabilityRows []*cdp.Node
 	err := chromedp.Run(ctx, chromedp.Navigate(URL),
-		//chromedp.Nodes(`document.querySelector("#primary-table > tbody > tr:nth-child(7) > th > a")`, &nodes, chromedp.ByJSPath)
-		chromedp.Nodes(`document.querySelector("#primary-table > tbody > tr:nth-child(8) > th")`, &nodes, chromedp.ByJSPath),
+		chromedp.Nodes("table.primary-table > tbody > tr.service-row > th", &serviceRows, chromedp.ByQueryAll),
+		chromedp.Nodes("table.primary-table > tbody > tr.capability-row > th", &capabilityRows, chromedp.ByQueryAll),
+		chromedp.ActionFunc(func(c context.Context) error {
+			// depth -1 for the entire subtree
+			// do your best to limit the size of the subtree
+			for _, n := range serviceRows {
+				dom.RequestChildNodes(n.NodeID).WithDepth(-1).Do(c)
+			}
+			for _, n := range capabilityRows {
+				dom.RequestChildNodes(n.NodeID).WithDepth(-1).Do(c)
+			}
+			return nil
+		}),
 	)
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	for _, n := range nodes {
-		fmt.Println(n.Children[0].NodeValue)
+	// fmt.Println("=====" + "service-row" + "=====")
+	// fmt.Println(len(serviceRows))
+	for _, n := range serviceRows {
+		child := n.Children[0]
+		if child.ChildNodeCount != 0 {
+			child = child.Children[0]
+		}
+		fmt.Println(strings.TrimSpace(child.NodeValue))
+	}
+
+	// fmt.Println("=====" + "capability-row" + "=====")
+	// fmt.Println(len(capabilityRows))
+	for _, n := range capabilityRows {
+		child := n.Children[0]
+		if child.ChildNodeCount != 0 {
+			child = child.Children[0]
+		}
+		fmt.Println(strings.TrimSpace(child.NodeValue))
 	}
 }
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/atsushi-kitazawa/azure_service_list_golang
+
+go 1.15
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20220515234810-83d799542a04
+	github.com/chromedp/chromedp v0.8.1
+	golang.org/x/sys v0.0.0-20220517195934-5e4e11fc645e // indirect
+)
